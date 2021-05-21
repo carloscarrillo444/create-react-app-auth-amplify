4a2af754bf14a067ce9fea8f1bd5a3dccaa5f02e
ccar0026 <ccar0026@student.monash.edu>
2021-05-21 20:14:13 +1000

    commit 12

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   selectFile(event) {
     console.log('selectFile - Starting');
-    this.setState({
+    this.State({
       currentFile: event.target.files[0],
       previewImage: URL.createObjectURL(event.target.files[0]),
       progress: 0,

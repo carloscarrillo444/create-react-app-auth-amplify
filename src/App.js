8dd600eb62b589605a860f24142c729e648234d7
ccar0026 <ccar0026@student.monash.edu>
2021-05-21 22:03:08 +1000

    commit 19

--- src/App.js
+++ src/App.js
@@ -21,11 +21,31 @@ class App extends Component {
       file: URL.createObjectURL(event.target.files[0])
     })
   }
+
+  async handleClick() {
+
+    console.log('handleClick - Starting');
+    console.log('handleClick - this.state.file:' + this.state.file);
+    const response = await fetch('https://27e4ccrsxd.execute-api.us-east-1.amazonaws.com/default/uploadImageToBucket');
+    const data = await response.json();
+    console.log('Response: ', data)
+    console.log('Uploading: ', this.state.file)
+    let binary = atob(this.state.file.split(',')[1])
+    let array = []
+    for (var i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i))
+    }
+    let blobData = new Blob([new Uint8Array(array)], {type: 'image/jpeg'})
+    console.log('Uploading to: ', response.data.uploadURL)
+  }
+
+
   render() {
     return (
       <div>
         <input type="file" onChange={this.handleChange}/>
         <img src={this.state.file}/>
+        <button v-if="!uploadURL" onClick={this.handleClick}>Upload image</button>
       </div>
     );
   }

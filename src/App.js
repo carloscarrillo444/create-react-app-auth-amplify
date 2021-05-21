89807823828286225c6b93ddca49bde60ea215f2
ccar0026 <ccar0026@student.monash.edu>
2021-05-21 22:10:48 +1000

    commit 20

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
@@ -9,46 +10,84 @@ Amplify.configure(aws_exports);
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      file: null
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(event) {
-    this.setState({
-      file: URL.createObjectURL(event.target.files[0])
-    })
-  }
-
-  async handleClick() {
-
-    console.log('handleClick - Starting');
-    console.log('handleClick - this.state.file:' + this.state.file);
-    const response = await fetch('https://27e4ccrsxd.execute-api.us-east-1.amazonaws.com/default/uploadImageToBucket');
-    const data = await response.json();
-    console.log('Response: ', data)
-    console.log('Uploading: ', this.state.file)
-    let binary = atob(this.state.file.split(',')[1])
-    let array = []
-    for (var i = 0; i < binary.length; i++) {
-      array.push(binary.charCodeAt(i))
-    }
-    let blobData = new Blob([new Uint8Array(array)], {type: 'image/jpeg'})
-    console.log('Uploading to: ', response.data.uploadURL)
-  }
-
-
-  render() {
-    return (
-      <div>
-        <input type="file" onChange={this.handleChange}/>
-        <img src={this.state.file}/>
-        <button v-if="!uploadURL" onClick={this.handleClick}>Upload image</button>
-      </div>
-    );
-  }
+  state = { 
+  
+    // Initially, no file is selected 
+    selectedFile: null
+  }; 
+   
+  // On file select (from the pop up) 
+  onFileChange = event => { 
+    // Update the state 
+    this.setState({ selectedFile: event.target.files[0] }); 
+  }; 
+   
+  // On file upload (click the upload button) 
+  onFileUpload = () => { 
+    // Create an object of formData 
+    const formData = new FormData(); 
+   
+    // Update the formData object 
+    formData.append( 
+      "myFile", 
+      this.state.selectedFile, 
+      this.state.selectedFile.name 
+    ); 
+   
+    // Details of the uploaded file 
+    console.log(this.state.selectedFile); 
+   
+    // Request made to the backend api 
+    // Send formData object 
+    axios.post("api/uploadfile", formData); 
+  }; 
+   
+  // File content to be displayed after 
+  // file upload is complete 
+  fileData = () => { 
+    if (this.state.selectedFile) { 
+        
+      return ( 
+        <div> 
+          <h2>File Details:</h2> 
+          <p>File Name: {this.state.selectedFile.name}</p> 
+          <p>File Type: {this.state.selectedFile.type}</p> 
+          <p> 
+            Last Modified:{" "} 
+            {this.state.selectedFile.lastModifiedDate.toDateString()} 
+          </p> 
+        </div> 
+      ); 
+    } else { 
+      return ( 
+        <div> 
+          <br /> 
+          <h4>Choose before Pressing the Upload button</h4> 
+        </div> 
+      ); 
+    } 
+  }; 
+   
+  render() { 
+    return ( 
+      <div> 
+          <h1> 
+            GeeksforGeeks 
+          </h1> 
+          <h3> 
+            File Upload using React! 
+          </h3> 
+          <div> 
+              <input type="file" onChange={this.onFileChange} /> 
+              <button onClick={this.onFileUpload}> 
+                Upload! 
+              </button> 
+          </div> 
+        {this.fileData()} 
+      </div> 
+    ); 
+  } 
+} 
 }
 
 export default withAuthenticator(App, true);

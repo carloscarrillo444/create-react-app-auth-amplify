3e97859260f0cbb9f84d09957bfab5d54b6b5ce2
ccar0026 <ccar0026@student.monash.edu>
2021-05-23 11:46:02 +1000

    6

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
               axios.get("https://27e4ccrsxd.execute-api.us-east-1.amazonaws.com/default/uploadImageToBucket")
               .then(response => {
-                console.log('Date created with comillas: ', response[0]).uploadURL;
+                console.log('Date created with comillas: ', response[0].uploadURL);
               });
 
               
